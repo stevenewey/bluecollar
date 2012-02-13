1a42ff1afca4e204b6add933f93133a20b0bfcb8
Stephen Newey <s@s-n.me>
2012-02-13 00:57:16 +0000

    Simplify prototype. Identify function by traversing dot notation.

--- bluecollar/prototype.py
+++ bluecollar/prototype.py
@@ -9,19 +9,7 @@
 # any imports?
 
 
-class Prototype(object):
-    """Prototype class for exposing your code via Redis"""
-
-    def __init__(self):
-        """
-        Default options
-        reusable: True
-
-        """
-        self._reusable = True
-
-    @property
-    def reusable(self):
-        """reusable property getter"""
-        return self._reusable
+class Cacheable(object):
+    """Prototype class for a cacheable class"""
+    pass
 
--- bluecollar/worker.py
+++ bluecollar/worker.py
@@ -22,6 +22,7 @@ import redis
 import gevent
 
 # bluecollar modules
+import prototype
 
 # our PID will be checked in redis to see if we should die
 _PID = os.getpid()
@@ -71,6 +72,43 @@ def clean_exit(*args):
     sys.exit(0)
 signal.signal(signal.SIGTERM, clean_exit)
 
+def route_to_class_or_function(path, module):
+    """Follow the dot-notation string to find the class or function"""
+    # maintain route to module for submodule imports
+    full_path = module.__name__
+    items = path.split('.')
+    if (hasattr(module, items[0]) and
+            ((callable(getattr(module, items[0])) and
+                len(items) == 1) or
+            (type(getattr(module, items[0])) is type and
+                len(items) == 2 and
+                hasattr(getattr(module, items[0]), items[1]) and
+                callable(getattr(getattr(module, items[0]), items[1]))))):
+        # this module contains a callable function or class that contains
+        # a callable method and there's no leftover items
+        return getattr(module, items[0])
+    if (hasattr(module, items[0]) and
+            len(items) > 1 and
+            hasattr(getattr(module, items[0]), items[1])):
+        # we can find the next item down, so go look in there
+        return route_to_class_or_function(
+            '.'.join(items[1:]),
+            getattr(module, items[0]))
+    # try and load submodule and carry on
+    if len(items) > 1:
+        submodule = None
+        try:
+            submodule = __import__(
+                    '%s.%s' % (full_path, items[0]),
+                    fromlist=[str(items[1])])
+        except ImportError:
+            pass
+        if submodule:
+            return route_to_class_or_function(
+                '.'.join(items[1:]),
+                submodule)
+    return False
+
 # main loop
 if __name__ == '__main__':
     # big old exception catcher for redis failing
@@ -106,8 +144,42 @@ if __name__ == '__main__':
                     type(REQUEST['request'][0]) not in [unicode, str]):
                 logging.error('Missing or invalid request: %s', REQUEST)
                 continue
+            METHOD = REQUEST['request'][0]
 
-            logging.info('Request: %s', REQUEST)
+            # attempt to resolve the requested function
+            # keeping a cache of them along the way
+            if _EXEC_CACHE.has_key(METHOD):
+                EXECUTABLE = _EXEC_CACHE[METHOD]
+            else:
+                EXECUTABLE = route_to_class_or_function(
+                    METHOD, MODULE)
+                _EXEC_CACHE[METHOD] = EXECUTABLE
+                if not EXECUTABLE:
+                    logging.error('Failed to find class or function at %s',
+                        METHOD)
+                    continue
+
+            # instantiate if we're dealing with a class
+            if type(EXECUTABLE) is type:
+                if issubclass(EXECUTABLE, prototype.Cacheable):
+                    # inherits cacheable, we only need one
+                    if not _INST_CACHE.has_key(METHOD):
+                        # we don't have one yet, so make one
+                        _INST_CACHE[METHOD] = EXECUTABLE()
+                        logging.debug('New cacheable instance: %s',
+                                _INST_CACHE[METHOD])
+                    INSTANCE = _INST_CACHE[METHOD]
+                else:
+                    # instantiate a regular class every call
+                    INSTANCE = EXECUTABLE()
+                    logging.debug('New instance: %s', INSTANCE)
+                # get the instance method we care about
+                FUNC = getattr(INSTANCE, METHOD.split('.')[-1])
+            else:
+                # a normal function (outside a class)
+                FUNC = EXECUTABLE
+
+            logging.info('Requested class/func: %s', FUNC)
 
     except redis.exceptions.ConnectionError, message:
         # redis isn't there or went away
